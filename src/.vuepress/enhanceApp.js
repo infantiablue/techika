20f5741991ac01cdda1e5099ca0e30768bed56b5
Truong Phan <dangtruong@gmail.com>
2021-01-25 00:04:20 +0700

    Apply Dark Mode based on device setting

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -6,9 +6,9 @@ export default ({
 }) => {
 	//set up dark theme
 	require("./styles/dark.css");
-	if (!("theme" in localStorage)) localStorage.theme = "dark";
-	options.created = function() {
-		let htmlElm = document.querySelector("html");
-		localStorage.theme === "light" ? htmlElm.classList.remove("dark") : htmlElm.classList.add("dark");
-	};
+	// if (!("theme" in localStorage)) localStorage.theme = "light";
+	// options.created = function() {
+	// 	let htmlElm = document.querySelector("html");
+	// 	localStorage.theme === "dark" ? htmlElm.classList.add("dark") : htmlElm.classList.remove("dark");
+	// };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			whitelistPatternsChildren: [/^language-/, /^token/, /^pre/, /^code/],
 		},
 	},
-	darkMode: "class", // or 'media' or 'class'
+	darkMode: "media", // or 'media' or 'class'
 	theme: {
 		extend: {
 			colors: {
