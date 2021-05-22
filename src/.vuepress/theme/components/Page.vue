ad54aeec40178efb0427f5bf3746584579d626dd
Truong Phan <dangtruong@gmail.com>
2021-05-22 22:19:56 +0700

    Darkmode for twitter icon

--- src/.vuepress/theme/components/Page.vue
+++ src/.vuepress/theme/components/Page.vue
@@ -7,10 +7,11 @@
 				<div class="text-sm mr-2" v-if="$page.frontmatter.date">📅 {{ new Date(Date.parse($page.frontmatter.date)).toDateString() }}</div>
 				<div class="text-sm mr-2" v-if="$page.frontmatter.author">✍️ {{ $page.frontmatter.author }}</div>
 				<a v-if="$page.frontmatter.author" class="twitter-share-button" :href="`https://twitter.com/intent/tweet?text=${$page.title}&url=https://techika.com${$page.path}`">
-					<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
-						<path
-							d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-						/></svg
+					<span
+						><svg class="fill-current text-gray-900 dark:text-blue-500  " xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+							<path
+								d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+							/></svg></span
 				></a>
 			</div>
 			<div class="my-2" v-if="$page.frontmatter.image"><img :src="$page.frontmatter.image" :alt="$page.title" width="1024" height="195" /></div>
@@ -28,15 +29,15 @@
 				name="revue-form"
 				target="_blank"
 			>
-				<h3 class="text-blue-900">Latest articles right to your inbox. Once per month.</h3>
+				<h3 class="text-blue-900">Meet Techika Newsletter with useful content on web development.</h3>
 				<div class="flex gap-4 items-end flex-wrap">
 					<div class="revue-form-group">
-						<!-- <label for="member_email">Email</label> -->
 						<input class="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email" />
 					</div>
 					<div class="revue-form-actions">
 						<input type="submit" class="bg-green-600 text-white p-2 rounded-md" value="Subscribe" name="member[subscribe]" id="member_submit" />
 					</div>
+					<div class="text-sm text-gray-100">Once a month. Unsubscribe anytime.</div>
 				</div>
 			</form>
 		</div>
